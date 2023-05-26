edb2a03e99fd39c70c72c5a62afbf2d257ea24bc
Yrlan R Teixeira <yrlan.01@hotmail.com>
2023-05-26 17:27:56 -0300

    fix: modal and cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,19 +1,17 @@
-import './card.css'
+import "./card.css";
 
 interface CardProps {
-    price: string | number;
-    title: string;
-    image: string;
-  }
-  
-  export function Card({ price, title, image }: CardProps) {
-    return (
-        <div className="card">
-        <img src={image} alt={title} />
-        <h2>{title}</h2>
-        <p className="card__price-label">Valor: {price}</p>
-      </div>
-      
-    );
-  }
-  
+  price: string | number;
+  title: string;
+  image: string;
+}
+
+export function Card({ price, title, image }: CardProps) {
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p className="card__price-label">Valor: {price}</p>
+    </div>
+  );
+}
